d253c5b3ffcd721086c700a2d0063c679b7be931
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-07-29 21:06:24 +0100

    NW_01A
    ##CODE
    + Used Debug.Log instead of print to write to console

---
+++ Number Wizard Console/Assets/NumberWizardControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizardControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Please pick a number");
+        Debug.Log("It must be equal to or greater than 1");
+        Debug.Log("It must be equal to or less than 1000");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
