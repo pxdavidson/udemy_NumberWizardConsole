c015ed1fa66e1ffcb825e789d47f6b92f7d75a4a
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-07-30 23:11:08 +0100

    NW_02A
    ##CODE
    + Enabled CrossPlatformInput assetpack and referenced in C# code
    + Added if statement to handle input
    + Added switch statement to also handle input
    // TODO: resolve to switch statement to process input

--- Number Wizard Console/Assets/NumberWizardControl.cs
+++ Number Wizard Console/Assets/NumberWizardControl.cs
@@ -1,21 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class NumberWizardControl : MonoBehaviour {
 
-	// Use this for initialization
+    // Declare Variables
+    int maxGuess = 1000;
+    int minGuess = 1;
+    
+    // Use this for initialization
 	void Start ()
     {
+       
+
         Debug.Log("Welcome to Number Wizard");
         Debug.Log("Please pick a number");
-        Debug.Log("It must be equal to or greater than 1");
-        Debug.Log("It must be equal to or less than 1000");
+        Debug.Log("It must be equal to or greater than " + minGuess);
+        Debug.Log("It must be equal to or less than " + maxGuess);
     }
 	
-	// Update is called once per frame
-	void Update ()
+	// TODO: Clean the update methos for handling input to be a switch
+    
+    // Update is called once per frame
+	public void Update ()
     {
-		
-	}
+        // testing Switch
+        var selectedKey = Input.inputString;
+        switch (selectedKey)
+        {
+            case "A":
+                Debug.Log("You Pressed A");
+                break;
+            case "B":
+                Debug.Log("You Pressed B");
+                break;
+        }
+
+        // Using Ifs
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("You Selected Higher");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("You Selected Lower");
+        }
+    }
 }
