8330b8ca80d68b444803f4c17d4432a394387223
pxdavidson <38045508+pxdavidson@users.noreply.github.com>
2018-08-11 22:20:36 +0100

    NW_03A
    ##CODE
    + MakeAGuess()
    + Cleaned up broken switch and used an if instead
    
    ##DATA
    - SampleScene
    + NumberWizardScene

--- Number Wizard Console/Assets/NumberWizardControl.cs
+++ Number Wizard Console/Assets/NumberWizardControl.cs
@@ -8,16 +8,16 @@ public class NumberWizardControl : MonoBehaviour {
     // Declare Variables
     int maxGuess = 1000;
     int minGuess = 1;
+    int guess = 500;
     
     // Use this for initialization
 	void Start ()
     {
-       
-
         Debug.Log("Welcome to Number Wizard");
         Debug.Log("Please pick a number");
         Debug.Log("It must be equal to or greater than " + minGuess);
         Debug.Log("It must be equal to or less than " + maxGuess);
+        MakeAGuess();
     }
 	
 	// TODO: Clean the update methods for handling input to be a switch
@@ -25,32 +25,34 @@ public class NumberWizardControl : MonoBehaviour {
     // Update is called once per frame
 	public void Update ()
     {
-        // testing Switch
-        var selectedKey = Input.inputString;
-        switch (selectedKey)
-        {
-            case "a":
-                Debug.Log("You Pressed A");
-                break;
-            case "b":
-                Debug.Log("You Pressed B");
-                break;
-            case "UpArrow":
-                Debug.Log("You Selected Higher");
-                break;
-            case "DownArrow":
-                Debug.Log("You Selected Lower");
-                break;
-        }
-
         // Using Ifs
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("You Selected Higher");
+            Debug.Log("It is higher.");
+            minGuess = guess;
+            guess = minGuess - guess * 2;
+            MakeAGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("You Selected Lower");
+            Debug.Log("It is lower");
+            maxGuess = guess;
+            guess = maxGuess - guess / 2;
+            MakeAGuess();
         }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Your number is " + guess);
+        }
+        else
+        {
+            //Do nothing
+        }
+    }
+
+    void MakeAGuess ()
+    {
+
+        Debug.Log("Is your number higher or lower than " + guess + "?");
     }
 }
