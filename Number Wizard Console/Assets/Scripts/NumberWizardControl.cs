1dd5068be01ec6c4428a573e3a3570076199e303
Phil Davidson <pxdavidson@msn.com>
2019-02-08 00:07:09 +0000

    NW_07A
    Refactored code and set up for button press and GUI display.

--- Number Wizard Console/Assets/Scripts/NumberWizardControl.cs
+++ Number Wizard Console/Assets/Scripts/NumberWizardControl.cs
@@ -9,12 +9,12 @@ public class NumberWizardControl : MonoBehaviour
 {
 
     // Declare Variables
-    int maxGuess;
-    int minGuess;
+    [SerializeField] int maxGuess;
+    [SerializeField] int minGuess;
     int playerGuess;
 
     //Other Scripts
-    UITextRenderer uITextRenderer;
+    Text textRenderer;
 
     // Use this for initialization
     void Start ()
@@ -26,10 +26,7 @@ public class NumberWizardControl : MonoBehaviour
     // Used to define the global variables used
     void DefineVariables()
     {
-        uITextRenderer = FindObjectOfType<UITextRenderer>();
-        maxGuess = 1000;
-        minGuess = 1;
-        playerGuess = 500;
+        textRenderer = FindObjectOfType<Text>();
         maxGuess++;
     }
 
@@ -37,42 +34,20 @@ public class NumberWizardControl : MonoBehaviour
     private void ProcessGuess()
     {
         playerGuess = (minGuess + maxGuess) / 2;
-        uITextRenderer.ParseText("Is your number higher or lower than " + playerGuess + "?");
     }
 
-    // Update is called once per frame
-    public void Update ()
+    // Number is lower
+    public void OnPressLower()
     {
-        ProcessInput();
+        maxGuess = playerGuess;
+        ProcessGuess();
     }
 
-    // Process user input
-    private void ProcessInput()
+    // Number is higher
+    public void OnPressHigher()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            minGuess = playerGuess;
-            ProcessGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            maxGuess = playerGuess;
-            ProcessGuess();
-        }
-        else if (Input.GetKeyUp(KeyCode.Return))
-        {
-            uITextRenderer.ParseText("Your number is " + playerGuess + " press Space to play again");
-            
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            Start();
-        }
-        else
-        {
-            //Do nothing
-        }
+        minGuess = playerGuess;
+        ProcessGuess();
     }
-
 
 }
